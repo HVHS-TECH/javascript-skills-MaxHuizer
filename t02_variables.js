879f498a6e142e47d08e0865e1cac6ad7f96a1bf
MaxHuizer <24069mh@hvhs.school.nz>
2026-04-28 23:57:16 +0000

    Apr 28, 2026, 11:57 PM

--- t02_variables.js
+++ t02_variables.js
@@ -5,14 +5,15 @@ Variables task 1
 //Variables
 var userName = "Max"
 var age = 15;
-var money = "$0"
+var pocketMoney = "$0"
 var year = "2026"
 
 
 /************************** 
 Main Code
 ***************************/
-console.log("Hi" + userName)
+console.log("Hi " + userName)
+console.log("As of " + year "you are " + age "years old.")
 
 
 
