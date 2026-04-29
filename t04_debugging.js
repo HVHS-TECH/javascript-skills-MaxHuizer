a1f6c114afabd6f819214ce4fc29e6b14f6a5148
MaxHuizer <24069mh@hvhs.school.nz>
2026-04-29 00:28:51 +0000

    variables and math

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,4 +1,4 @@
-konsole.log["Task 4 - Debugging")
+console.log["Task 4 - Debugging")
 
 // calculate the area
 var area = length + width;
@@ -8,11 +8,11 @@ var perimeter = 2  length + 2 * width;
 
 // Set up the size of the page.
 var length = 8;
-var width = 10
+var width = 10;
 
 // Display the results
-console.log("A area of a piece of paper with a length of "+length+", and a width of "+width+" is "+ area)
-console.log("The same piece of paper has a perimeter of "+perimeter)
+console.log("A area of a piece of paper with a length of "+ length + ", and a width of " + width + " is " + area)
+console.log("The same piece of paper has a perimeter of "+ perimeter)
 
 // Check, does this result look sensible
 if (perimeter > area){
