dc24507d1af3735bc1723e2909730c7af1c495ac
MaxHuizer <24069mh@hvhs.school.nz>
2026-04-29 00:19:18 +0000

    Apr 29, 2026, 12:19 AM

--- t03_maths.js
+++ t03_maths.js
@@ -7,7 +7,6 @@ var userName = "Max"
 var age = 15;
 var newAge = age + 10;
 var pocketMoney = "200"
-var newPocketMoney = pocketMoney /2
 var year = "2026"
 
 
@@ -18,7 +17,7 @@ console.log("Hi " + userName)
 console.log("As of " + year + " you are " + age + " years old")
 console.log("You have $" + pocketMoney + " dollars")
 console.log("In 10 years you will be " + newAge + " years old")
-console.log("You have $" + newPocketMoney + " dollars")
+console.log("You have $" + pocketMoney + " dollars")
 console.log("You spend half your money now you have $" + newPocketMoney)
 
 
