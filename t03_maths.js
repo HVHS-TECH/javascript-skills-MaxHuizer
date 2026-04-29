bec872c8d5cbbb78b9c02a849594a283f5f48114
MaxHuizer <24069mh@hvhs.school.nz>
2026-04-29 00:10:58 +0000

    Apr 29, 2026, 12:10 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,26 @@
-/* Paste your code from task 2 here */
+/****************************
+Variables task 1
+****************************/
+
+//Variables
+var userName = "Max"
+var age = 15;
+var newAge = age + 10;
+var pocketMoney = "0"
+var year = "2026"
+
+
+/************************** 
+Main Code
+***************************/
+console.log("Hi " + userName)
+console.log("As of " + year + " you are " + age + " years old")
+console.log("You have $" + pocketMoney + " dollars")
+console.log("In 10 years you will be " + newAge + " years old")
+console.log("You have $" + pocketMoney)
+
+
+
+/*****************************
+ Functions
+******************************//* Paste your code from task 2 here */
