592f965e546235252e455f34ea2f725588fe010e
MaxHuizer <24069mh@hvhs.school.nz>
2026-04-30 01:22:39 +0000

    Apr 30, 2026, 1:22 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,32 @@
-/* Paste your code from task 3, Maths here */
+/* Paste your code from task 3, Maths here *//****************************
+Variables task 1
+****************************/
+
+//Variables
+var userName = "Max"
+var age = 15;
+var newAge = age + 10;
+var pocketMoney = 200;
+var year = "2026"
+
+
+/************************** 
+Main Code
+***************************/
+console.log("Hi " + userName)
+console.log("As of " + year + " you are " + age + " years old")
+console.log("You have $" + pocketMoney + " dollars")
+console.log("In 10 years you will be " + newAge + " years old")
+pocketMoney = pocketMoney + 100;
+console.log("You have $" + pocketMoney + " dollars")
+pocketMoney = pocketMoney/2;
+console.log("You spend half your money now you have $" + pocketMoney)
+pocketMoney = pocketMoney + 3;
+console.log("Then you get $3, now you have $ " + pocketMoney)
+
+
+
+/*****************************
+ Functions
+******************************/
+
