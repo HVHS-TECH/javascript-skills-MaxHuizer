1df534c3d1e559d4c7773b0bc13fd40f1270382f
MaxHuizer <24069mh@hvhs.school.nz>
2026-04-30 01:04:03 +0000

    Apr 30, 2026, 1:04 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -4,7 +4,7 @@ console.log["Task 4 - Debugging"]
 var area = length + width;
 
 // calculate the perimeter
-var perimeter = 2  length + 2 width ; 
+var perimeter = 2 * length + 2 * width; 
 
 // Set up the size of the page.
 var length = 8;
@@ -18,5 +18,5 @@ console.log("The same piece of paper has a perimeter of "+ perimeter)
 if (perimeter > area){
     console.log("Yay, your area is larger, this is right for this example")
 }else{
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example)
+    console.log("Hmm, your perimeter is larger. It shouldn't be for this example")
 }
