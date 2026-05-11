5c28ff7138e6b6bbf3c48c4e18f3fbfe9c4d2aa1
MaxHuizer <24069mh@hvhs.school.nz>
2026-05-11 21:42:32 +0000

    May 11, 2026, 9:42 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -53,3 +53,7 @@ function displayProduct(_name, _price){
 OUTPUT.innerHTML += "<p>Welcome to the shop</p>"
 OUTPUT.innerHTML += "<p>" + _name + ": $" + _price+"</p>"
 }
+
+function getFormInput(){
+    const NAME_FIELD = document.getElementById
+}
