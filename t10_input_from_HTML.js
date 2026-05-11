3fb2a6d84b89aa9bc69f687fe6e148f1c1338ffa
MaxHuizer <24069mh@hvhs.school.nz>
2026-05-11 21:33:03 +0000

    May 11, 2026, 9:33 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -57,4 +57,5 @@ OUTPUT.innerHTML += "<p>" + _name + ": $" + _price+"</p>"
 function getFormInput(){
     const NAME_FIELD = document.getElementById("nameField");
     let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
 }
